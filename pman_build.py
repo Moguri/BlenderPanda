ba2289d51dc44060302de8a2e44f0db4c895962c
Mitchell Stokes <mogurijin@gmail.com>
2016-03-29 21:44:11 -0700

    pman: Copy non-blend files when building

--- pman_build.py
+++ pman_build.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import shutil
 
 import bpy
 
@@ -10,8 +11,19 @@ print('Exporting:', srcdir)
 print('Export to:', dstdir)
 
 for asset in os.listdir(srcdir):
+    src = os.path.join(srcdir, asset)
+    dst = os.path.join(dstdir, asset)
+
+    if os.path.exists(dst) and os.stat(src).st_mtime <= os.stat(dst).st_mtime:
+        print('Skip building up-to-date file: {}'.format(dst))
+        continue
+
     if asset.endswith('.blend'):
-        frompath = os.path.join(srcdir, asset)
+        print('Converting .blend file ({}) to .bam ({})'.format(src, dst))
         topath = os.path.join(dstdir, asset.replace('.blend', '.bam'))
-        bpy.ops.wm.open_mainfile(filepath=frompath)
-        bpy.ops.panda_engine.export_bam(filepath=topath)
+        dst = dst.replace('.blend', '.bam')
+        bpy.ops.wm.open_mainfile(filepath=src)
+        bpy.ops.panda_engine.export_bam(filepath=dst)
+    else:
+        print('Copying non .blend file from "{}" to "{}'.format(src, dst))
+        shutil.copyfile(src, dst)
