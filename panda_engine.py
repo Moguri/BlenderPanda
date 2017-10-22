d81f55ff94c33ff86d28ef9d9d5e2bf13c077544
Mitchell Stokes <mogurijin@gmail.com>
2017-10-22 13:49:22 -0700

    panda_engine: Cleanup lint warnings

--- panda_engine.py
+++ panda_engine.py
@@ -1,13 +1,10 @@
 import os
-import sys
 
 
 import bpy
 from .brte.brte import engine
 from .brte.brte.processors import ExternalProcessor
-
-import pman
-
+from . import pman
 
 
 class PandaEngine(bpy.types.RenderEngine, engine.RealTimeEngine):
@@ -17,7 +14,7 @@ class PandaEngine(bpy.types.RenderEngine, engine.RealTimeEngine):
     def __init__(self):
         try:
             config = pman.get_config(os.path.dirname(bpy.data.filepath) if bpy.data.filepath else None)
-        except pman.NoConfigError as e:
+        except pman.NoConfigError as _err:
             config = None
         pycmd = pman.get_python_program(config)
         path = os.path.join(os.path.dirname(__file__), 'processor_app.py')
